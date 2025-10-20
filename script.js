80fc2a8cd8be51c11084f34c74c81c1fc5372a07
Vahenoor <singhvahenoor18@gmail.com>
2025-10-20 10:37:15 +0530

    v1

---
+++ script.js
@@ -0,0 +1,28 @@
+const datepicker = require('js-datepicker');
+const { DateTime, Duration } = require('luxon');
+
+let luxonDate;
+let selectedDate;
+const ans = document.getElementById("answer");
+
+const picker = datepicker('#my-date-input', {
+    dateSelected: new Date(),
+    position: 'bl', // 'br' for bottom right (one of many options) // Example: Restrict selection to dates after Jan 1, 2023
+    onSelect: (instance, date) => {
+        // This function runs every time a date is selected
+        console.log(`Selected date is: ${date.toLocaleDateString()}`);
+        selectedDate = date;
+        luxonDate = DateTime.fromJSDate(selectedDate);
+    },
+    maxDate: new Date(),
+    formatter: (input, date, instance) =>{
+        const value = date.toLocaleDateString();
+        input.value = value;
+    }
+});
+function calculate(){
+    let difference = DateTime.now().diff(luxonDate,['years', 'months']).toObject();
+    console.log(`You are ${difference.years} years and ${Math.floor(difference.months)} months old`)
+    ans.textContent = `You are ${difference.years} years and ${Math.floor(difference.months)} months old`;
+}
+window.calculate = calculate;
